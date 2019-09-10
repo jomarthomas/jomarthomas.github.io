4ffaa253b9ec20648cbf1761c7d079a1da52f691
Jomar <12853402+jomarthomas@users.noreply.github.com>
2019-09-10 17:04:02 -0400

    migrated photo site
    Temp solution

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+    //Mail Function
+    $("form").on("submit", function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "https://formcarry.com/s/Y2j7afa-r1c",
+            dataType: "text",
+            data: $('form').serialize(),
+            success: function () {
+                $('#confirmEmail').modal('show')
+            }
+        });
+        // console.log($(this).serialize());
+    });
+
+    //Footer Year display
+    let d = new Date();
+    let y = d.getFullYear();
+    $("#year").text(y);
+
+    //Macy options
+    setTimeout(() => {
+        Macy({
+            container: ".gallery",
+            trueOrder: false,
+            waitForImages: false,
+            margin: 14,
+            columns: 3,
+            breakAt: {
+                1200: 3,
+                940: 2,
+                400: 1
+            }
+        });
+    }, 250);
+
+    //For Gallery 4 only 
+    setTimeout(() => {
+        Macy({
+            container: ".gallery2",
+            trueOrder: false,
+            waitForImages: false,
+            margin: 14,
+            columns: 3,
+            breakAt: {
+                1200: 3,
+                940: 2,
+                400: 1
+            }
+        });
+    }, 250);
+
+    //Macy and Mansory Setup
+    // var pathName = window.location.pathname;
+    // var match = pathName.match(/\d/g);
+    // var number = match[0];
+    // var folder = `../../../assets/images/gallery${number}/`;
+
+    // $.ajax({
+    //     url: folder,
+    //     success: function (data) {
+    //         $(data)
+    //             .find("a")
+    //             .attr("href", function (i, val) {
+    //                 if (val.match(/\.(jpe?g|jpg|png|gif)$/)) {
+    //                     $(".gallery").append(
+    //                         "<img src='" + folder + val + "'>"
+    //                     );
+    //                 }
+    //             });
+    //     }
+    // });
+
+
+
+    // $(".carousel").carousel({
+    //     interval: 2000
+    // });
+});
